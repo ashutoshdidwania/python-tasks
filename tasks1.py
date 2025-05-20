2494edfd4b29c13be9ce78936c882fb21e4df475
Ashutosh Didwania <167608490+ashutoshdidwania@users.noreply.github.com>
2025-05-20 21:52:19 +0530

    Add tasks1.py

--- tasks1.py
+++ tasks1.py
@@ -1,7 +1,5 @@
-a=float(input("Enter the first number:"))
-b=float(input("Enter the second number:"))
-print("Addition:",a+b)
-print("Subtraction:",a-b)
-print("Multiplication:",a*b)
-print("Division:",a/b)
-
+a=int(input("Enter the number:"))
+if a%2==0:
+    print(str(a) + " is an even number")
+else:
+    print(str(a) + " is an odd number")
