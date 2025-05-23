042f56ee5d5a21ea2649439c861593eaa4a40034
Ashutosh Didwania <167608490+ashutoshdidwania@users.noreply.github.com>
2025-05-23 15:05:46 +0530

    Add task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,20 @@
-sum = 0
-for i in range(1, 51):
-    sum += i
+# sum = 0
+# for i in range(1, 51):
+#     sum += i
+#
+# print("The sum of numbers from 1 to 50 is:", sum)
+from math import *
+n=int(input("Enter the number:"))
+print("Square Root:", sqrt(n))
+print("Logarithm:", log(n))
+print("Sine:", sin(n))
 
-print("The sum of numbers from 1 to 50 is:", sum)
+
+
+
+
+
+
+
+
+
