3da6d5b002f7b77e271dccfe13fc5860676c60cb
Ashutosh Didwania <167608490+ashutoshdidwania@users.noreply.github.com>
2025-05-23 15:04:14 +0530

    Add tasks1.py

--- tasks1.py
+++ tasks1.py
@@ -1,5 +1,14 @@
-a=int(input("Enter the number:"))
-if a%2==0:
-    print(str(a) + " is an even number")
-else:
-    print(str(a) + " is an odd number")
+# a=int(input("Enter the number:"))
+# if a%2==0:
+#     print(str(a) + " is an even number")
+# else:
+#     print(str(a) + " is an odd number")
+
+n=int(input("Enter the number:"))
+def fact(n):
+    if n<2:
+        return 1
+    else:
+        return n * (fact(n-1))
+sample_number = fact(5)
+print("Factorial of 5 is", sample_number)
