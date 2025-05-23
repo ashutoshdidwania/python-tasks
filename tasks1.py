8244903edb79b11371a0e3a72dda7bf88e610aab
Ashutosh Didwania <167608490+ashutoshdidwania@users.noreply.github.com>
2025-05-23 16:23:12 +0530

    Add tasks1.py

--- tasks1.py
+++ tasks1.py
@@ -1,14 +1,7 @@
-# a=int(input("Enter the number:"))
-# if a%2==0:
-#     print(str(a) + " is an even number")
-# else:
-#     print(str(a) + " is an odd number")
+try:
+    file=open('sample.txt','r')
+    reading_file=file.read()
+    print(reading_file)
+except:
+    print("The file sample.txt was not found")
 
-n=int(input("Enter the number:"))
-def fact(n):
-    if n<2:
-        return 1
-    else:
-        return n * (fact(n-1))
-sample_number = fact(5)
-print("Factorial of 5 is", sample_number)
